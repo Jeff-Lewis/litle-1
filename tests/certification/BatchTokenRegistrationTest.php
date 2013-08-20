e46d15a9dd4525479363886189d9cb8374838659
Nate Krantz <nkrantz23@gmail.com>
2013-08-20 13:36:37 -0400

    Tweaking token registration code to hit proper cert environment

--- tests/certification/BatchTokenRegistrationTest.php
+++ tests/certification/BatchTokenRegistrationTest.php
@@ -11,7 +11,7 @@ class BatchTokenRegistrationTest extends CertificationTestCase {
 	public function testCreateBatchOfTokensReturnsArray() {
 		$reg    = new Batch\TokenRegistration(static::getParams());
 		$result = $reg->send(static::cards());
-
+var_export($result);
 		$this->assertInternalType('array', $result);
 	}
 
@@ -22,19 +22,19 @@ class BatchTokenRegistrationTest extends CertificationTestCase {
 		return [
 			[	
 				'payment_option_id' => '123456',
-				'card' => '10000000001041'
+				'card' => '4005101001000002'
 			],
 			[			
 				'payment_option_id' => '123457',
-				'card' => '10000000001042'
+				'card' => '4005101001000003'
 			],
 			[	
 				'payment_option_id' => '123458',
-				'card' => '10000000001043'
+				'card' => '4005101001000004'
 			],
 			[	
 				'payment_option_id' => '123459',
-				'card' => '10000000001044'
+				'card' => '4005101001000005'
 			]
 		];
 	}
