fbd76ef885f013da4e97d7f57aa5b1a274212488
Nate Krantz <nkrantz23@gmail.com>
2013-08-20 14:25:44 -0400

    Added more token cert tests

--- tests/certification/TokenRegistrationTest.php
+++ tests/certification/TokenRegistrationTest.php
@@ -9,15 +9,49 @@ class TokenRegistrationTest extends CertificationTestCase {
 
 	/**
 	 * Successful Token Registraiton
+	 *
+	 * (note once this executes once, it will return 'already registered' since 
+	 *  litles cert environment resets daily and not per request)
 	 */
-	public function testSuccessfulTokenRegistration() {
+	public function testSuccessfulTokenRegistrationNewlyRegistered() {
 		$response = (new Request\RegisterTokenRequest(static::getParams()))->make([
 			'orderId' => '1',
 			'accountNumber' => '4457119922390123'
 		]);
+
 		$this->assertTrue($response->isApproved());
 		$this->assertEquals('0123', substr($response->getLitleToken(), -4));
 		$this->assertEquals('VI', $response->getCardType());
 		$this->assertEquals('445711', $response->getCardBin());
 	}
+
+	/**
+	 * Invalid CC
+	 */
+	public function testFailedTokenRegistrationDueToInvalidCC() {
+		$response = (new Request\RegisterTokenRequest(static::getParams()))->make([
+			'orderId' 		=> '2',
+			'accountNumber' => '4457119999999999'
+		]);
+		
+		$this->assertTrue(!$response->isApproved());
+		$this->assertEquals(null, $response->getLitleToken());
+		$this->assertEquals('820', $response->getCode());
+	}
+
+	/**
+	 * Already Registered
+	 */
+	public function testSuccessfulTokenRegistrationAlreadyRegistered() {
+		$response = (new Request\RegisterTokenRequest(static::getParams()))->make([
+			'orderId' => '1',
+			'accountNumber' => '4457119922390123'
+		]);
+
+		$this->assertTrue($response->isApproved());
+		$this->assertEquals('0123', substr($response->getLitleToken(), -4));
+		$this->assertEquals('VI', $response->getCardType());
+		$this->assertEquals('445711', $response->getCardBin());
+	}
+
 }
