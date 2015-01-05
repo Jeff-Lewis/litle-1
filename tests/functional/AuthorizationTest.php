a638624ca09a3285a8e81eb43b616dfa2cf2b78e
Cory Crowther <cory@petflow.com>
2015-01-05 15:43:54 -0500

    spacing

--- tests/functional/AuthorizationTest.php
+++ tests/functional/AuthorizationTest.php
@@ -7,72 +7,72 @@ use Petflow\Litle\Transaction\Request\AuthorizationRequest;
  */
 class AuthorizationTest extends FunctionalTestCase {
 
-	/**
-	 * Test Approved Authorization
-	 */
-	public function testApprovedAuthorization() 
-	{
-		$authorization = (new AuthorizationRequest(static::getParams(), []))->make(static::transactions('approved'));
+    /**
+     * Test Approved Authorization
+     */
+    public function testApprovedAuthorization() 
+    {
+        $authorization = (new AuthorizationRequest(static::getParams(), []))->make(static::transactions('approved'));
 
-		$this->assertEquals('22', $authorization->getOrderId());
-		$this->assertEquals('01', $authorization->getAVS()['code']);
-		$this->assertEquals('000', $authorization->getCode());
-	}
+        $this->assertEquals('22', $authorization->getOrderId());
+        $this->assertEquals('01', $authorization->getAVS()['code']);
+        $this->assertEquals('000', $authorization->getCode());
+    }
 
-	/**
-	 * Test the account updater element
-	 */
-	public function testAccountUpdater() {
-		$authorization = (new AuthorizationRequest(static::getParams(), []))->make(static::transactions('account_updater'));
-		$this->assertArrayHasKey('original', $authorization->getUpdaterElement());
-		$this->assertArrayHasKey('corrected', $authorization->getUpdaterElement());
+    /**
+     * Test the account updater element
+     */
+    public function testAccountUpdater() {
+        $authorization = (new AuthorizationRequest(static::getParams(), []))->make(static::transactions('account_updater'));
+        $this->assertArrayHasKey('original', $authorization->getUpdaterElement());
+        $this->assertArrayHasKey('corrected', $authorization->getUpdaterElement());
 
-		$this->assertNotEmpty($authorization->getUpdaterElement()['original']);
-		$this->assertNotEmpty($authorization->getUpdaterElement()['corrected']);
+        $this->assertNotEmpty($authorization->getUpdaterElement()['original']);
+        $this->assertNotEmpty($authorization->getUpdaterElement()['corrected']);
 
-		$this->assertTrue($authorization->cardRequiresUpdate());
-	}
+        $this->assertTrue($authorization->cardRequiresUpdate());
+    }
 
-	/**
-	 * Transactions
-	 */
-	public static function transactions($key)
-	{
-		$trans = [
-			'approved' => [	
-				'id' 		=> '22',
-				'orderId' 	=> '22',
-				'amount' 	=> '1000',
-				'card'   	=> [
-					'number' 	=> '374322062409525',
-					'type' 		=> 'AX',
-					'expDate' 	=> '0315',
-					'cardValidationNumber' => ''
-				],
-				'billToAddress' => [
-					'name' => 'Johnny Sucks',
-					'zip' => 12561
-				]
-			],
+    /**
+     * Transactions
+     */
+    public static function transactions($key)
+    {
+        $trans = [
+            'approved' => [ 
+                'id'        => '22',
+                'orderId'   => '22',
+                'amount'    => '1000',
+                'card'      => [
+                    'number'    => '374322062409525',
+                    'type'      => 'AX',
+                    'expDate'   => '0315',
+                    'cardValidationNumber' => ''
+                ],
+                'billToAddress' => [
+                    'name' => 'Johnny Sucks',
+                    'zip' => 12561
+                ]
+            ],
 
-			'account_updater' => [
-				'id' 		=> '22',
-				'orderId' 	=> '22',
-				'amount' 	=> '1000',
-				'card'   	=> [
-					'number' 	=> '4100117890123000',
-					'type' 		=> 'AX',
-					'expDate' 	=> '0315',
-					'cardValidationNumber' => ''
-				],
-				'billToAddress' => [
-					'name' => 'Johnny Sucks',
-					'zip' => 12561
-				]
-			]
-		];
+            'account_updater' => [
+                'id'        => '22',
+                'orderId'   => '22',
+                'amount'    => '1000',
+                'card'      => [
+                    'number'    => '4100117890123000',
+                    'type'      => 'AX',
+                    'expDate'   => '0315',
+                    'cardValidationNumber' => ''
+                ],
+                'billToAddress' => [
+                    'name' => 'Johnny Sucks',
+                    'zip' => 12561
+                ]
+            ]
+        ];
 
-		return $trans[$key];
-	}
+        return $trans[$key];
+    }
 
 }
